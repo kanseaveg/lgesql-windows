e6fbb168c9be1d558ec4cf6eb7301ae91568fd63
kanseaveg <taroballscai@hotmail.com>
2023-02-03 18:56:08 +0800

    fix submission problems

--- run/windows/run_submission.bat
+++ run/windows/run_submission.bat
@@ -1,7 +1,7 @@
-saved_model=saved_models\%1
-output_path=saved_models\%1\predicted_sql.txt
-batch_size=10
-beam_size=5
+set saved_model=saved_models\%1
+set output_path=saved_models\%1\predicted_sql.txt
+set batch_size=10
+set beam_size=5
 set current_dir=%cd%
 
 python eval.py --db_dir %current_dir%\data\database --table_path %current_dir%\data\tables.json --dataset_path %current_dir%\data\dev.json --saved_model %saved_model% --output_path %output_path% --batch_size %batch_size% --beam_size %beam_size%
